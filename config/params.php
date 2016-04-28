3240802b84f43fff05913ae93098181f39796004
Tallinus <polyglotdnb@gmail.com>
2016-04-28 04:54:53 -0700

    This is a start state of my project

---
+++ config/params.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'adminEmail' => 'scimax@yandex.ru',
+];
---
+++ views/site/gallery.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Gallery';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-gallery">
+
+  <h1>Hello <?= Html::encode($target) ?></h1>
+  <p>Welcome to your Yii2 demonstration application.</p>
+
+</div>
